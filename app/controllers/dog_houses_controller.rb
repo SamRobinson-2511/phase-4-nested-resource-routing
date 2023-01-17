28b435902305e480cdc6cfbac43d79301e8a9be6
Sam Robinson <110217039+SamRobinson-2511@users.noreply.github.com>
2023-01-17 12:26:27 -0600

    commit tree 872690e84087af24d89787c52f649a0c9c869d44

--- app/controllers/dog_houses_controller.rb
+++ app/controllers/dog_houses_controller.rb
@@ -2,14 +2,14 @@ class DogHousesController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
   def show
-    dog_house = DogHouse.find(params[:id])
-    render json: dog_house
+    dog_house = DogHouse.find_by(id:params[:id])
+    render json: dog_house, status: :ok
   end
 
   private
 
   def render_not_found_response
-    render json: { error: "Dog house not found" }, status: :not_found
+    render json: { errors: dog_house.errors.full_messages}, status: 422
   end
 
 end
